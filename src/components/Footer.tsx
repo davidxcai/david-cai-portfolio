56a3d7b8845caf5b965523708ff04fbf9d010a20
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-05-02 18:47:01 -0500

    Added copy button component to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,12 @@
-import { Button, Text, Tooltip } from "@mantine/core";
-import { useClipboard } from "@mantine/hooks";
+import { Button, CopyButton, Text, Tooltip } from "@mantine/core";
 import {
     IconBrandGithub,
     IconBrandLinkedin,
     IconCopy,
+    IconCheck,
 } from "@tabler/icons-react";
 
 export function Footer() {
-    const clipboard = useClipboard({ timeout: 500 });
     return (
         <footer className=" text-white p-4 mt-auto border-t-1 border-dashed border-gray-700">
             <div className="max-w-7xl mx-auto p-4 flex flex-row items-stretch flex-wrap">
@@ -18,33 +17,46 @@ export function Footer() {
                 </div>
                 <div className="w-1/2 flex flex-col gap-4 ">
                     <Button
-                        leftSection={<IconBrandLinkedin size={14} />}
+                        leftSection={<IconBrandLinkedin size={16} />}
                         variant="transparent"
+                        component="a"
+                        href="https://www.linkedin.com/in/davidxiencai/"
+                        target="_blank"
                     >
                         LinkedIn
                     </Button>
                     <Button
-                        leftSection={<IconBrandGithub size={14} />}
+                        leftSection={<IconBrandGithub size={16} />}
                         variant="transparent"
+                        component="a"
+                        href="https://github.com/davidxcai"
+                        target="_blank"
                     >
                         GitHub
                     </Button>
-                    <Tooltip
-                        label={
-                            clipboard.copied ? "Copied" : "Copy to clipboard"
-                        }
-                    >
-                        <Button
-                            color={clipboard.copied ? "teal" : "blue"}
-                            onClick={() =>
-                                clipboard.copy("davidxcai@gmail.com")
-                            }
-                            leftSection={<IconCopy size={14} />}
-                            variant="transparent"
-                        >
-                            Email
-                        </Button>
-                    </Tooltip>
+                    <CopyButton value="davidxcai@gmail.com">
+                        {({ copied, copy }) => (
+                            <Tooltip
+                                label={copied ? "Copied!" : "Copy to clipboard"}
+                                withArrow
+                            >
+                                <Button
+                                    color={copied ? "teal" : "blue"}
+                                    onClick={copy}
+                                    leftSection={
+                                        copied ? (
+                                            <IconCheck size={16} />
+                                        ) : (
+                                            <IconCopy size={16} />
+                                        )
+                                    }
+                                    variant="transparent"
+                                >
+                                    Email
+                                </Button>
+                            </Tooltip>
+                        )}
+                    </CopyButton>
                 </div>
             </div>
         </footer>
