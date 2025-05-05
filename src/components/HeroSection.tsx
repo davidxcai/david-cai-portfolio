9f0b7dfdfc16ba1c711f4ff70ebfb2d76dc4f72d
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-05-04 20:23:16 -0500

    Updated resume button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex } from "@mantine/core";
+import { Flex } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { ProfilePicture } from "../assets/ProfilePicture";
 import { NavLink } from "react-router-dom";
@@ -14,24 +14,29 @@ export function HeroSection() {
             className="mb-16 md:mb-24"
         >
             <ProfilePicture />
-            <div className="flex flex-col flex-wrap gap-4 w-full md:w-1/2">
-                <h1 className="text-7xl sm:text-9xl font-bold nanum-text text-center md:text-left">
-                    I'm David
-                </h1>
-                <p className="text-gray-400 text-3xl sm:text-5xl nanum-text text-center md:text-left">
-                    A fullstack web developer & computer science student
-                </p>
+            <div className="flex flex-col flex-wrap gap-12 w-full md:w-1/2">
+                <div>
+                    <h1 className="text-7xl sm:text-9xl font-bold nanum-text text-center md:text-left">
+                        I'm David
+                    </h1>
+                    <h3 className="text-gray-400 text-3xl sm:text-5xl nanum-text text-center md:text-left">
+                        A fullstack web developer & computer science student
+                    </h3>
+                </div>
                 {isMobile && (
-                    <Button
-                        variant="outline"
-                        color="white"
-                        component={NavLink}
+                    <NavLink
                         to="/resume"
+                        className="bg-transparent border-1 border-white hover:border-indigo-500 hover:text-indigo-500 py-2 rounded-sm font-bold text-center"
                     >
                         Resume
-                    </Button>
+                    </NavLink>
                 )}
             </div>
         </Flex>
     );
+}
+{
+    /* <Button variant="outline" color="white" component={NavLink} to="/resume">
+    Resume
+</Button>; */
 }
